4233e91fdea30b7ceee3fb65c1010a50a270350a
fate-me <jcfateme.87@gmail.com>
2025-04-26 03:16:03 +0330

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("tamrin 1");
+for (let s = 1; s <= 150; s++) {
+    console.log(s);
+}
+
+console.log("tamrin 2");
+let a = 2;
+let sum = 0;
+while (a <= 100) {
+    sum = sum + a;
+    a += 2;
+}
+console.log(sum);
+
+console.log("tamrin 3");
+let f = 1;
+for (let i = 10; i >= 1; i--) {
+    f *= i;
+}
+console.log(f);

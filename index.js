03f909e0858276d7167b37bbaa82352216945485
fate-me <jcfateme.87@gmail.com>
2025-04-26 03:27:33 +0330

    fix bug

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ for (let s = 1; s <= 150; s++) {
 }
 
 console.log("tamrin 2");
+let f = 1;
+for (let i = 10; i >= 1; i--) {
+    f *= i;
+}
+console.log(f);
+
+console.log("tamrin 3");
 let a = 2;
 let sum = 0;
 while (a <= 100) {
@@ -11,10 +18,3 @@ while (a <= 100) {
     a += 2;
 }
 console.log(sum);
-
-console.log("tamrin 3");
-let f = 1;
-for (let i = 10; i >= 1; i--) {
-    f *= i;
-}
-console.log(f);
